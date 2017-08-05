ddbb9dc4b92bf87d0732b0f261b2ec79d06c5510
btotharye <brianhh1230@gmail.com>
2017-08-05 08:23:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Geek Hue skill
+
+This skill is a revamp of the existing hue skill using fuzzy matching for turning groups on and colors eventually, etc.
+
+To get this done we need
+  - phue
+
+
+
+## Current state
+
+Working features:
+ - ...
+
+Known issues:
+ - ...
+
+TODO:
+ - ...
---
+++ __init__.py
@@ -0,0 +1,61 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Visit https://docs.mycroft.ai/skill.creation for more detailed information
+# on the structure of this skill and its containing folder, as well as
+# instructions for designing your own skill based on this template.
+
+
+# Import statements: the list of outside modules you'll be using in your
+# skills, whether from other files in mycroft-core or from external libraries
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from phue import Bridge
+from fuzzywuzzy import fuzz
+
+__author__ = 'brihopki'
+
+# Logger: used for debug lines, like "LOGGER.debug(xyz)". These
+# statements will show up in the command line when running Mycroft.
+LOGGER = getLogger(__name__)
+
+# The logic of each skill is contained within its own class, which inherits
+# base methods from the MycroftSkill class with the syntax you can see below:
+# "class ____Skill(MycroftSkill)"
+class GeekHueSkill(MycroftSkill):
+    @intent_handler(IntentBuilder('GroupLightIntent').require("GroupLightKeyword").require('group').build())
+    def handle_group_light(self, message):
+        group = message.data['group']
+        action = message.data['action']
+        LOGGER.debug("The group is {} and the action is {}".format(group, action))
+
+
+    # The "stop" method defines what Mycroft does when told to stop during
+    # the skill's execution. In this case, since the skill's functionality
+    # is extremely simple, the method just contains the keyword "pass", which
+    # does nothing.
+    def stop(self):
+        pass
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+def create_skill():
+    return GeekHueSkill()
