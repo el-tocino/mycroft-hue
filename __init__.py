8dfc217c41f34dfb49f7dd898429cfa2a77aac42
btotharye <brianhh1230@gmail.com>
2017-08-05 10:39:24 -0400

    adding in actions to turn lights on or off

--- __init__.py
+++ __init__.py
@@ -77,16 +77,25 @@ class GeekHueSkill(MycroftSkill):
         group_on = group[1]
         group_name = group[0]
         group_lights = group[2]
+        group_id = ''
         if action == 'on':
             if group_on == False:
                 LOGGER.debug("The group we would turn {} is {}".format(action, group_name))
+                group_id = bridge.get_group_id_by_name(group_name)
+                bridge.set_group(group_name, 'on', True)
+                self.speak("Turned {} group {}".format(action, group_name))
             else:
                 LOGGER.debug("Group {} is already {}".format(group_name, action))
+                self.speak("Group {} is already {}".format(group_name, action))
         else:
             if group_on == True:
                 LOGGER.debug("The group we would turn {} is {}".format(action, group_name))
+                group_id = bridge.get_group_id_by_name(group_name)
+                bridge.set_group(group_name, 'on', False)
+                self.speak("Turned {} group {}".format(action, group_name))
             else:
                 LOGGER.debug("The group {} is already {}".format(group_name, action))
+                self.speak("Group {} is already {}".format(group_name, action))
 
 
 
