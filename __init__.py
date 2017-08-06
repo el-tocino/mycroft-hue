01cceeadfca4093e45eeeb76fb0d6768cb3d17e7
btotharye <brianhh1230@gmail.com>
2017-08-06 09:18:12 -0400

    updating speak for all lights on/off

--- __init__.py
+++ __init__.py
@@ -89,7 +89,7 @@ class GeekHueSkill(MycroftSkill):
         bridge = _connect_bridge(self.bridge)
         if phrase_group == 'all lights':
             all_lights_on_off(bridge, action)
-            self.speak("Turned off all lights")
+            self.speak("Turned {} all lights".format(action))
         else:
             group = get_group_name(bridge, phrase_group)
             group_on = group[1]
