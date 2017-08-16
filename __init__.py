9a17dfcb2d3da66a77ef156d8a4b2dd46a031d8d
btotharye <brianhh1230@gmail.com>
2017-08-16 06:15:34 -0400

    adding in color green

--- __init__.py
+++ __init__.py
@@ -85,7 +85,7 @@ def change_group_color(bridge, group, color):
     elif color == 'purple':
         bridge.set_group(hue_group[0], 'xy', [0.2217, 0.1126])
         LOGGER.debug("Setting group {} to color {}".format(hue_group[0], color))
-    elif color == 'green'
+    elif color == 'green':
         bridge.set_group(hue_group[0], 'xy', [0.409, 0.518])
         LOGGER.debug("Setting group {} to color {}".format(hue_group[0], color))
     else:
