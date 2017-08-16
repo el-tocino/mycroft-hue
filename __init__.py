e0492e1336479d53f446b66b4a157f5d50a2e514
btotharye <brianhh1230@gmail.com>
2017-08-16 06:04:41 -0400

    adding debugging

--- __init__.py
+++ __init__.py
@@ -72,7 +72,7 @@ def all_lights_on_off(bridge, action):
 
 def change_group_color(bridge, group, color):
     hue_group = get_group_name(bridge, group)
-    LOGGER.debug(hue_group)
+    LOGGER.debug("This is the hue group: {}".format(hue_group))
     if color == 'red':
         bridge.set_group(hue_group, 'xy', [0.704, 0.296])
         LOGGER.debug("Setting group {} to color {}".format(hue_group, color))
